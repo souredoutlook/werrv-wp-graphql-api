567960d7dbd02b5ae9819304e3a298bff9a5508e
souredoutlook <souredoutlook@gmail.com>
2021-06-15 22:31:53 -0400

    Handle missing fields

--- routes/index.js
+++ routes/index.js
@@ -12,37 +12,41 @@ module.exports = (fetch) => {
   router.post('/', function(req, res) {
 
     console.log(req.body);
-    
+
     const { identifier, discountCodeName, codeType } = req.body;
 
-    const variables = generateVariables(identifier, discountCodeName, codeType)
-
-    fetch(`https://${SHOP}.myshopify.com/admin/api/graphql.json`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Shopify-Access-Token": ACCESS_TOKEN
-    },
-    body: JSON.stringify({ query, variables })
-  })
-    .then(result => {
-      return result.json();
-    })
-    .then(json => {
-      const { data } = json;
-
-      if (data && data.discountCodeBasicCreate) {
-        const { codeDiscountNode, userErrors } = data.discountCodeBasicCreate;
-
-        if (codeDiscountNode) {
-          //modify this to send the discount code and discount code url
-          res.send(codeDiscountNode);
-        } else {
-          //design decision: send the first error, as errors are resolved the list will shrink to zero
-          res.send(userErrors[0].message);
+    if (identifier, discountCodeName, codeType) {
+      const variables = generateVariables(identifier, discountCodeName, codeType)
+  
+      fetch(`https://${SHOP}.myshopify.com/admin/api/graphql.json`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": ACCESS_TOKEN
+      },
+      body: JSON.stringify({ query, variables })
+      })
+      .then(result => {
+        return result.json();
+      })
+      .then(json => {
+        const { data } = json;
+  
+        if (data && data.discountCodeBasicCreate) {
+          const { codeDiscountNode, userErrors } = data.discountCodeBasicCreate;
+  
+          if (codeDiscountNode) {
+            //modify this to send the discount code and discount code url
+            res.send(codeDiscountNode);
+          } else {
+            //design decision: send the first error, as errors are resolved the list will shrink to zero
+            res.send(userErrors[0].message);
+          }
         }
-      }
-    });
+      });
+    } else {  
+      res.send("Some required fields are missing.")
+    }
   });
 
   return router;
