02160e126e276aa37f9ca5f360d213d4d2715e0f
souredoutlook <souredoutlook@gmail.com>
2021-06-18 12:54:05 -0400

    add cors support

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const fetch = require("node-fetch");
 
@@ -9,6 +10,7 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
