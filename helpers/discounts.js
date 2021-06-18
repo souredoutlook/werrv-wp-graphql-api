75a174bfa022c96bdb854c694d6341ca9bf3e2c4
souredoutlook <souredoutlook@gmail.com>
2021-06-18 12:54:39 -0400

    align lookup hash with wpforms values

--- helpers/discounts.js
+++ helpers/discounts.js
@@ -19,29 +19,33 @@ const query = `
 const generateVariables = function(identifier, discountCodeName, codeType) {
   //the unique attributes of each type of discount, as outlined in the specifications
   const constants = {
-    A: { appliesOncePerCustomer: true, percentage: 0.1 },
-    B: { appliesOncePerCustomer: false, percentage: 0.05 },
-    C: { appliesOncePerCustomer: false, percentage: 0.1 },
+    '10% Off Single Use': { appliesOncePerCustomer: true, percentage: 0.1 },
+    '5% Off All Orders': { appliesOncePerCustomer: false, percentage: 0.05 },
+    '10% Off All Orders': { appliesOncePerCustomer: false, percentage: 0.1 },
   };
 
-  const { appliesOncePerCustomer, percentage } = constants[codeType];
-
-  return {
-    "basicCodeDiscount": {
-      "customerSelection": { "all": true },
-      // new discount codes are timestamped with a string representing the date in ISO 8601 Extended Format as per Shopify docs
-      "startsAt": new Date(Date.now()).toISOString(),
-      "title": `${identifier}.${discountCodeName}`,
-      "code": discountCodeName,
-      appliesOncePerCustomer,
-      "customerGets": {
-          "items": {"all": true },
-          "value": { percentage } 
-      },
-      //usageLimit defines the maximum number of times a code can be used by any customer
-      "usageLimit": null,
-    }
-  };
+  if (constants[codeType]) {
+    const { appliesOncePerCustomer, percentage } = constants[codeType];
+  
+    return {
+      "basicCodeDiscount": {
+        "customerSelection": { "all": true },
+        // new discount codes are timestamped with a string representing the date in ISO 8601 Extended Format as per Shopify docs
+        "startsAt": new Date(Date.now()).toISOString(),
+        "title": `${identifier}.${discountCodeName}`,
+        "code": discountCodeName,
+        appliesOncePerCustomer,
+        "customerGets": {
+            "items": {"all": true },
+            "value": { percentage } 
+        },
+        //usageLimit defines the maximum number of times a code can be used by any customer
+        "usageLimit": null,
+      }
+    };
+  } else {
+    return null;
+  }
 };
 
 module.exports = { query, generateVariables};
